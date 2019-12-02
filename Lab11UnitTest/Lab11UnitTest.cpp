cd142a6ba847c8b812a09d8d625aa65c018aff78
Jakob Wardega <wardegjn@mail.uc.edu>
2019-12-02 09:58:27 -0500

    added 2 easy unit tests

--- Lab11UnitTest/Lab11UnitTest.cpp
+++ Lab11UnitTest/Lab11UnitTest.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "..///Lab11HashesDLL/Lab11HashesDLL.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +10,43 @@ namespace Lab11UnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(getLength1)
+		{
+			hashTable myTable(100);
+			myTable.addItem(50, 5);
+			myTable.addItem(51, 5);
+			int length = myTable.getLength();
+			Assert::AreEqual(length, 2);
+
+		}
+		TEST_METHOD(getLength2)
+		{
+			hashTable myTable(100);
+			myTable.addItem(50, 5);
+			myTable.addItem(51, 5);
+			myTable.removeItem(51);
+			myTable.removeItem(50);
+			int length = myTable.getLength();
+			Assert::AreEqual(length, 0);
+		}
+		TEST_METHOD(getVal1)
 		{
 		}
+		TEST_METHOD(getVal2)
+		{
+		}
+		TEST_METHOD(removeItem1)
+		{
+		}
+		TEST_METHOD(gremoveItem2)
+		{
+		}
+		TEST_METHOD(addItem1)
+		{
+		}
+		TEST_METHOD(addItem2)
+		{
+		}
+
 	};
 }
